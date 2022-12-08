3ef2e96338a208198d0a9ac1777074d94b7a7fd4
Shalom <shalomamos9@gmail.com>
2022-12-08 11:45:26 -0800

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - -entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: 0 after printing the function
+ */
 
 int main(void)
 
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
