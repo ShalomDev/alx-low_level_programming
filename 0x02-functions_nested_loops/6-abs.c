cd9984e1401fa518449534927be3e0cc39cdd65b
Shalom <shalomamos9@gmail.com>
2022-12-15 09:19:47 -0800

    1-8

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _abs - output absolute value of integer input
+ * @n: integer to check
+ *
+ * Return: n
+ */
+
+int _abs(int n)
+
+{
+
+		if (n >= 0)
+
+					return (n);
+
+
+
+			else
+
+					{
+
+								n *= -1;
+
+										return (n);
+
+											}
+
+}
