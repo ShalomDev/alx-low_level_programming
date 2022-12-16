81cb265d6e1978de2b220a0bf756c8986f542705
Shalom <shalomamos9@gmail.com>
2022-12-15 22:42:38 -0800

    1-4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9, followed by a new line.
+ * Return: void.
+ */
+
+{
+
+		int i;
+
+
+
+			for (i = 48; i < 58; i++)
+
+					{
+
+								_putchar(i);
+
+									}
+
+				_putchar('\n');
+
+}
